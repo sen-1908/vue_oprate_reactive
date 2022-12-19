d27556218788c4ceda3f42fa9afbf9dafab0b0b3
sen <sen121108@icloud.com>
2022-12-19 18:50:38 +0900

    makeReactive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,51 @@
-<template>
-  <div>
-    {{ item }}
-  </div>
-</template>
-
-<script>
+<script setup>
 import axios from 'axios'
-export default {
-  data () {
-    return {
-      item: []
-    }
-  },
-  methods: {
-    getItem () {
-      const path = 'http://localhost:8888/items'
+import { onBeforeMount, reactive } from 'vue';
+
+let items = reactive({});
+
+let testArray =reactive([]);
+let testObject =reactive({});
+
+
+console.log(items);
+console.log(testObject);
+console.log(testArray)
+onBeforeMount (() => {
+  testArray.values = [1,2,3];
+  testObject.values = {
+    item: 1,
+    itemt: 2
+  }
+
+  const path = 'http://localhost:8888/items'
       axios.get(path)
         .then(response => {
-          this.item = response.data
+       
+          items.values = response.data;
+      
         })
         .catch(error => {
           console.log(error)
-        })
-    }
-  },
-  created () {
-    this.getItem()
-  }
-}
+        });
+        
+})
+
 </script>
+
+<template>
+  <div>
+    <li v-for="item in testObject" :key="item.id">
+      {{ item }}
+      
+    </li>
+    <li v-for="item in testArray" :key="item.id">
+      {{ item }}
+      
+    </li>
+    <li v-for="item in items" :key="item.id">
+      {{ item }}
+      
+    </li>
+  </div>
+</template>
